07e76370941a801ad3a48e65cf30a6b345835338
Luksros <lukasrose@live.se>
2021-09-27 15:18:05 +0200

    Skapat metod som skriver ut menyn med hjälp av en array som finns i klassen Menu. Påbörjat skelett för att kunna navigera i menyn med en switch. Påbörjat klassen User, som kommer användas till att göra objekt att lagra olika användares information i.

--- Individuellt Projekt - Lukas R SUT21/Program.cs
+++ Individuellt Projekt - Lukas R SUT21/Program.cs
@@ -6,7 +6,59 @@ namespace Individuellt_Projekt___Lukas_R_SUT21
     {
         static void Main(string[] args)
         {
-
+            Menu MainMenu = new Menu();
+            MainMenu.PrintMenu("Lukas");
+            Console.ReadLine();
         }
+    }
+    class Menu
+    {
+        string[] menuChoices = { "1. Se dina konton och saldo", "2. Överföring mellan konton", "3. Ta ut pengar", "4. Logga ut" };
+
+        public void PrintMenu(string tempUserName)
+        {
+            Console.WriteLine("Välkommen, " + tempUserName);
+            Console.WriteLine();
+            for (int i = 0; i < menuChoices.Length; i++)
+            {
+                Console.WriteLine(menuChoices[i]);
+            }
+        }
+
+        public static void MenuSelection()
+        {
+            int selector = 0;
+            while (!int.TryParse(Console.ReadLine(), out selector))
+            {
+               
+            }
+            switch (selector)
+            {
+                case 1:
+                    {
+                        //Method for account overview
+                        break;
+                    }
+                case 2:
+                    {
+                        //Method for transferring funds between accounts
+                        break;
+                    }
+                case 3:
+                    {
+                        //Method for money withdrawals
+                        break;
+                    }
+                case 4:
+                    {
+                        //return to login screen
+                        break;
+                    }
+            }             
+            }
+    }
+    class User
+    {
+
     }
 }
